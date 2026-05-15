c48a861dfb9619b6826b27ad8873b7abe9b6e76f
goola971 <janviercharly97114@gmail.com>
2026-05-15 11:51:16 +0200

    ajout de header global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,27 @@
 import { Syne, Arimo } from 'next/font/google';
-import "./globals.css";
-import "@/lib/amplify";
+import '@/Components/header/header.css';
+import './globals.css';
+import '@/lib/amplify';
 
 const syne = Syne({
   subsets: ['latin'],
   variable: '--font-syne',
-  weight: ['700', '800']
+  weight: ['700', '800'],
 });
 
 const arimo = Arimo({
   subsets: ['latin'],
-  variable: '--font-arimo'
+  variable: '--font-arimo',
 });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="fr" className={`${syne.variable} ${arimo.variable} dark`}>
-      <body className="bg-[#050505] antialiased">
-        {children}
-      </body>
+    <html lang='fr' className={`${syne.variable} ${arimo.variable} dark`}>
+      <body className='bg-[#050505] antialiased'>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import { Header } from '@/components/Header';
-import { Hero } from '@/components/landing/Hero';
-import { Features } from '@/components/landing/Features';
-import { Avis } from '@/components/landing/Avis';
-import { Pv } from '@/components/landing/Pv';
-import { Faq } from '@/components/landing/Faq';
-import { Cta } from '@/components/landing/Cta';
-import { Footer } from '@/components/Footer';
+import { Header } from "@/components/header/Header";
+import { Hero } from "@/components/landing/Hero";
+import { Features } from "@/components/landing/Features";
+import { Avis } from "@/components/landing/Avis";
+import { Pv } from "@/components/landing/Pv";
+import { Faq } from "@/components/landing/Faq";
+import { Cta } from "@/components/landing/Cta";
+import { Footer } from "@/components/Footer";
 
 export default function Page() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-        <Pv />
-        <Features />
-        <Avis />
-        <Cta />
-        <Faq />
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<main>
+				<Hero />
+				<Pv />
+				<Features />
+				<Avis />
+				<Cta />
+				<Faq />
+			</main>
+			<Footer />
+		</>
+	);
 }
