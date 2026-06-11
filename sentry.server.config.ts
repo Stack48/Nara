73ce4ccd410f1eea7922ab69aa1002c6e946bee8
edepmatss <smgbadagni@gmail.com>
2026-06-11 20:43:58 +0200

    chore(sentry): configure sentry wizard and examples

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,9 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
+  dsn: "https://db4192d01f8bbbffaa70464debe555ef@o4511548228370432.ingest.de.sentry.io/4511548229812304",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
